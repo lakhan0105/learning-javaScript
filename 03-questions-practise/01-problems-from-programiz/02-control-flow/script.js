5edb44bdd68af81e70129ad19bc4c0374cde50f6
lakhan0105 <lakhan0105@gmail.com>
2023-10-22 18:08:57 +0530

    small changes

--- 03-questions-practise/01-problems-from-programiz/02-control-flow/script.js
+++ 03-questions-practise/01-problems-from-programiz/02-control-flow/script.js
@@ -1,6 +1,6 @@
 // COMMENT BEFORE RUNNING THE CODES
 
-// CONTROL FLOW ----------------------------------------------------------------------------------------------
+// CONTROL FLOW -------------------------------------------------------
 // PROGRAM TO SOLVE A QUADRAITIC EQUATION
 // - ax2 + bx + c = 0, where
 // - a,b,c = real numbers and a ≠ 0
@@ -49,7 +49,7 @@ if (discriminant > 0) {
 // a=1,b=-3,c=10
 // The roots of quadriatic equation are 1.50+2.78i and 1.50-2.78i
 
-// PROGRAM TO CHECK IF A NUM IS +ve/-ve/0 -------------------------------------------------------------------
+// PROGRAM TO CHECK IF A NUM IS +ve/-ve/0 --------------------------------
 const num = 0; // enter the number
 if (num > 0) {
   console.log(`The entered number ${num} is positive`);
@@ -59,7 +59,7 @@ if (num > 0) {
   console.log(`The entered number ${num} is -ve`);
 }
 
-// FIND IF THE NUMBER IF ODD/EVEN ----------------------------------------------------------------------------
+// FIND IF THE NUMBER IF ODD/EVEN -----------------------------------------
 const number = 1;
 if (number % 2 === 0) {
   console.log(`${number} is even number`);
@@ -67,7 +67,7 @@ if (number % 2 === 0) {
   console.log(`${number} is odd`);
 }
 
-// FIND THE GREATEST AMONG THREE NUMBERS ---------------------------------------------------------------------
+// FIND THE GREATEST AMONG THREE NUMBERS -----------------------------------
 // Ask user to enter the values of 3 numbers
 const num1 = 1;
 const num2 = 2;
@@ -81,4 +81,119 @@ if (num1 >= num2 && num1 >= num3) {
   console.log(`${num3} is the greatest`);
 }
 
-// 
+// CHECK IF A GIVEN NUMBER IS A PRIME OR NOT ----------------------------------
+// - any no divisible by 1 and itself
+// - 1 is neither composite nor prime
+
+function checkPrime(num) {
+  // check if (num<2) cuz  numbers <2 are not prime
+  if (num < 2) {
+    return `${num} is not a prime number`;
+  }
+
+  for (let i = 2; i < num; i++) {
+    // check if the num is divisible by any other num other than 1 and itself
+    if (num % i === 0) {
+      return `${num} is not a prime number`;
+    }
+  }
+  return `${num} is a prime number`;
+}
+console.log(checkPrime(2)); // prime number
+
+// PRINT ALL PRIME NOS IN AN INTERVAL -------------------------------------------
+function primeNumInterval(num1, num2) {
+  for (let i = num1; i <= num2; i++) {
+    let flag = 0;
+
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        flag = 1;
+        break;
+      }
+    }
+
+    // if num>1 and not divisible by other numbers
+    if (i > 1 && flag === 0) {
+      console.log(i);
+    }
+  }
+}
+primeNumInterval(1, 10); // 3 5 7
+
+// FIND FACTORIAL OF A NUMBER --------------------------------------------------------
+// - factorial n! - product of all the +ve integers from 1 to n,
+// - where n is not equal to -ve number (factorial of -ve num does not exist)
+// - factorial of 0 = 1
+
+function findFactorial(number) {
+  let fact = 1;
+
+  // check if num<0
+  if (num < 0) {
+    console.log("factorial of -ve number does not exist");
+  }
+
+  // loop and find the fact
+  for (let i = 1; i <= number; i++) {
+    fact *= i;
+  }
+  console.log(fact);
+}
+findFactorial(5); // 120
+
+// DISPLAY THE MULTIPLICATION TABLE -------------------------------------
+function mulTable(number) {
+  for (let i = 1; i <= 10; i++) {
+    console.log(`${number}x${i}=${number * i}`);
+  }
+}
+mulTable(5);
+
+// FIBONACCI SEQUENCE ---------------------------------------------------
+// - It is a series of nums in which each num (after first 2) is the sum of the two preceding ones.
+// - It typically starts with 0 and 1.
+// 0,1,1,2,3
+// 0,1 = 1
+// 1,1 = 2
+// 1,2 = 3
+// 2,3 = 5
+
+function printFibo(number) {
+  let n1 = 0;
+  let n2 = 1;
+  let nextTerm;
+  let fiboArray = [n1, n2];
+
+  for (let i = 0; i < number - 2; i++) {
+    // number-2 cuz we do not need extra 2 fib numbers printed
+    nextTerm = n1 + n2;
+    fiboArray.push(nextTerm);
+    n1 = n2;
+    n2 = nextTerm;
+  }
+  console.log(fiboArray);
+}
+
+printFibo(4); // [ 0, 1, 1, 2 ]
+
+// CHECK IF A GIVEN NUMBER IS A AMSTRONG NUMBER(of any length)
+// 153 = 1*1*1 + 5*5*5 + 3*3*3
+function amstrong(number) {
+  let temp = number;
+  let numLength = String(number).length;
+  let total = 0;
+
+  while (temp > 0) {
+    // we need to take the last digits one by one
+    let digit = temp % 10;
+    total += digit ** numLength;
+    temp = parseInt(temp / 10); // to remove the last digit from temp
+  }
+
+  if (total === number) {
+    console.log(`${number} is a amstrong number`);
+  }
+}
+amstrong(92727); // 92727 is a amstrong number
+
