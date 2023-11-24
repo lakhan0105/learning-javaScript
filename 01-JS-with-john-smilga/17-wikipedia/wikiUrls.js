ec810c3bece14eac004a9712d704f35b28946011
lakhan0105 <lakhan0105@gmail.com>
2023-11-25 03:03:36 +0530

    added some comments about url

--- 01-JS-with-john-smilga/17-wikipedia/wikiUrls.js
+++ 01-JS-with-john-smilga/17-wikipedia/wikiUrls.js
@@ -1,5 +1,10 @@
 // URL TO SEARCH
 // - const url = "https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=searchValue";
+// - list=search - perform a full text search
+// - srsearch="inputValue" - search for page titles or content matching  this value.
+// - srlimit=20 How many total pages to return.
+// - format=json json response
+// - "origin=*" fix cors errors
 
 // - we need to give the searchValue dynamically
 // - ex : https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=apple
